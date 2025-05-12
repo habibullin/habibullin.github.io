de89d5aa574f5ab2142f6b27b8520b5117d2e391
habibullin <rxletter@gmail.com>
2025-05-12 16:01:59 +0300

    fixed

--- assets/text-prepositions-fixer.py
+++ assets/text-prepositions-fixer.py
@@ -1,23 +1,23 @@
 import os
 import re
 
+WORDS_TO_REPLACE = ['и', 'в', 'на', 'с', 'по', 'к', 'из', 'для', 'от', 'а', 'о', 'но']
+PATTERN = r'\b(' + '|'.join(WORDS_TO_REPLACE) + r')\s+'
+
 def replace_spaces_in_file(file_path):
+    if not os.path.exists(file_path):
+        return  # Если файл не найден, просто выходим из функции
+
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    # Список предлогов и союзов
-    words_to_replace = ['и', 'в', 'на', 'с', 'по', 'к', 'из', 'для', 'от', 'а', 'о', 'но']
-    pattern = r'\b(' + '|'.join(words_to_replace) + r')\s+'
-    
-    # Заменяем пробелы на неразрывные
-    new_content = re.sub(pattern, lambda m: m.group(0).replace(' ', '\u00A0'), content)
+    if re.search(PATTERN, content):
+        # Заменяем пробелы на неразрывные
+        new_content = re.sub(PATTERN, lambda m: m.group(1) + '\u00A0', content)
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(new_content)
 
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(new_content)
-
-# Примените функцию ко всем нужным файлам
 for root, dirs, files in os.walk('es'):
     for file in files:
-        if file.endswith('.html') or file.endswith('.md'):  # Расширения файлов
+        if file.endswith('.html') or file.endswith('.md'):
             replace_spaces_in_file(os.path.join(root, file))
-
